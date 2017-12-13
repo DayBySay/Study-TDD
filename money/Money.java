5a6f5cbf8ec9586256d6aa6dbcf9af9bad5bdff4
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-12-13 12:45:49 +0900

    8章
    重複の除去の為にメソッドのシグネチャを合わせた

--- money/Money.java
+++ money/Money.java
@@ -1,10 +1,20 @@
 package money;
 
-public class Money {
+public abstract class Money {
     protected int amount;
 
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount && getClass().equals(money.getClass());
     }
+
+    static public Doller dollar(int amount) {
+        return new Doller(amount);
+    }
+
+    static public Franc franc(int amount) {
+        return new Franc(amount);
+    }
+
+    abstract Money times(int multiplier);
 }
